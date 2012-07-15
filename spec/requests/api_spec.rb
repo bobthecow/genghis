81d9c5c727850267f1a978303165d351a1289463
Justin Hileman <justin@justinhileman.info>
2012-07-14 17:09:35 -0700

    Add spec for /servers

--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -4,7 +4,8 @@ require 'faraday'
 describe 'Genghis API', :type => :request do
   before :all do
     @api = Faraday.new url: "http://localhost:#{@genghis_port}"
-    @api.headers['Accept'] = 'application/json'
+    @api.headers['Accept']       = 'application/json'
+    @api.headers['Content-Type'] = 'application/json'
   end
 
   it 'boots up' do
@@ -15,5 +16,28 @@ describe 'Genghis API', :type => :request do
     })
   end
 
-  it 'lists servers'
+  context 'servers' do
+    describe 'GET /servers' do
+      it 'lists available servers' do
+        res = @api.get '/servers'
+        res.status.should eq 200
+        res.body.should match_json_expression \
+          [
+            {
+              id:        'localhost',
+              name:      'localhost',
+              editable:  true,
+              size:      Fixnum,
+              count:     Fixnum,
+              databases: Array
+            }
+          ].ignore_extra_values!
+      end
+    end
+
+    describe 'POST /servers' do
+      it 'creates a server when given a valid DSN'
+      it 'returns 400 if the DSN is not valid'
+    end
+  end
 end
