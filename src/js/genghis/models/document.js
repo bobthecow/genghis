d428e8bfadd889c53d09c7a3c9c817cf326befe8
Justin Hileman <justin@justinhileman.info>
2012-09-20 13:18:15 -0700

    Use "prettyId" for document view titles.
     * If ObjectId, just the value string.
     * If BinData (subtype 3) UUID, a canonical hex UUID representation.
     * Otherwise, whatever to string makes it :P

--- src/js/genghis/models/document.js
+++ src/js/genghis/models/document.js
@@ -1,6 +1,6 @@
 Genghis.Models.Document = Backbone.Model.extend({
     initialize: function() {
-        _.bindAll(this, 'prettyPrint', 'JSONish');
+        _.bindAll(this, 'prettyId', 'prettyPrint', 'JSONish');
 
         var id = this.thunkId(this.get('_id'));
         if (id) {
@@ -36,6 +36,29 @@ Genghis.Models.Document = Backbone.Model.extend({
         if (this.isNew()) return base;
 
         return base + (base.charAt(base.length - 1) == '/' ? '' : '/') + encodeURIComponent(this.id);
+    },
+    prettyId: function() {
+        var id = this.get('_id');
+        if (typeof id == 'object' && id.hasOwnProperty('$genghisType')) {
+            switch (id['$genghisType']) {
+                case 'ObjectId':
+                    return id['$value'];
+
+                case 'BinData':
+                    // Special case: UUID
+                    if (id['$value']['$subtype'] == 3) {
+                        var uuid = /^([0-9a-f]{8})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{12})$/i;
+                        var hex  = Genghis.Util.base64ToHex(id['$value']['$binary']);
+                        if (uuid.test(hex)) {
+                            return hex.replace(uuid, '$1-$2-$3-$4-$5');
+                        }
+                    }
+
+                    return id['$value']['$binary'].replace(/=+$/, '');
+            }
+        }
+
+        return id;
     },
     prettyPrint: function() {
         return Genghis.JSON.prettyPrint(this.toJSON());
