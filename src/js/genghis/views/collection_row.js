d8caea61bf63df49b2623e63ee4f693cd79827db
Justin Hileman <justin@justinhileman.info>
2013-07-03 23:15:32 -0700

    Add UI support for emptying collections.
    Fixes #107

--- src/js/genghis/views/collection_row.js
+++ src/js/genghis/views/collection_row.js
@@ -1,4 +1,27 @@
 Genghis.Views.CollectionRow = Genghis.Views.BaseRow.extend({
     template: Genghis.Templates.CollectionRow,
-    isParanoid: true
+    isParanoid: true,
+    events: _.extend({
+        'click button.truncate': 'truncate'
+    }, Genghis.Views.BaseRow.prototype.events),
+    truncate: function() {
+        var model = this.model;
+        var name  = model.get('name');
+
+        new Genghis.Views.Confirm({
+            header: 'Remove all documents?',
+            body:   'Emptying this collection will remove all documents, but will leave indexes intact.' +
+                    '<br><br>Type <strong>'+name+'</strong> to continue:',
+            confirmInput: name,
+            confirmText:  'Empty '+name,
+            confirm: function() {
+                model.truncate({
+                    wait: true,
+                    success: function() {
+                        model.fetch();
+                    }
+                });
+            }
+        });
+    }
 });
